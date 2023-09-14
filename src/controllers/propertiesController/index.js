54ab396b391b22a041cf834f753fa68f96123caf
= <=>
2023-09-14 15:57:04 +0300

    fast api
    Signed-off-by: = <=>

--- src/controllers/propertiesController/index.js
+++
@@ -1,2 +0,0 @@
-import { defineproperty, getproperty, getproperties, updateproperty, deleteproperty, deleteproperties } from "../../database/properties";
-import { findAdminById } from './../../models/admin';
--- src/utilities/index.js
+++
@@ -1,7 +0,0 @@
-import crypto from 'crypto'
-const secret_variable = process.env.secret_variable
-
-export const genarateSaltedString = () => crypto.randomBytes(128).toString('base64');
-export const authentication = (salt, password) => {
-    return crypto.Hmac('sha256', [salt, password].join('/')).update(secret_variable).digest('hex')
-}
